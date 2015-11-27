e12775d3a3a65be6967a1b6a2bfb500c0f0b02c0
Mikael Lindqvist <li.mikael@gmail.com>
2015-11-27 10:32:27 +0300

    refactor to use class

---
+++ bin/stadiumpixels.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+function usage() {
+	console.log("stadiumpixels - Generate leaflets for human pixels in a stadium.");
+	console.log("");
+	console.log("Usage:");
+	console.log("  stadiumpixels [options] <files...>");
+	console.log("");
+	console.log("Options:");
+	console.log("  --outPrefix=<prefix>      - Specify out prefix for generated files.");
+	console.log("  --targetWidth=<width>     - Seats per row in the stadium.");
+	console.log("  --targetHeight=<height>   - Rows in the stadium.");
+	console.log("  --templateFile=<file>     - Swig template for generating leaflets.")
+	process.exit(1);
+}
+
+var minimist = require("minimist");
+var StadiumPixels = require("../src/StadiumPixels");
+
+var argv = minimist(process.argv.slice(2));
+
+if (!argv._.length) {
+	console.log("Usage: stadiumpixels <files...>");
+	process.exit(1);
+}
+
+var stadiumPixels = new StadiumPixels();
+
+for (k in argv) {
+	var v = argv[k];
+
+	switch (k) {
+		case "outPrefix":
+			stadiumPixels.setOutPrefix(v);
+			break;
+
+		case "targetWidth":
+			stadiumPixels.setTargetWidth(v);
+			break;
+
+		case "targetHeight":
+			stadiumPixels.setTargetHeight(v);
+			break;
+
+		case "templateFile":
+			stadiumPixels.setTemplateFile(v);
+			break;
+
+		case "_":
+			break;
+
+		default:
+			console.log("Unknown option: " + k);
+			process.exit(1);
+			return;
+	}
+}
+
+for (var i = 0; i < argv._.length; i++)
+	stadiumPixels.addImageFile(argv._[i]);
+
+stadiumPixels.run().then(function() {
+	console.log("Complete!");
+}, function(e) {
+	console.log(e);
+	process.exit(1);
+});
---
+++ src/StadiumPixels.js
@@ -0,0 +1,143 @@
+var fs = require("fs");
+var PNG = require('pngjs2').PNG;
+var swig = require("swig");
+var webshot = require('webshot');
+var async = require('async');
+var Thenable = require("tinp");
+
+function StaduimPixels() {
+	this.targetWidth = 48;
+	this.targetHeight = 20;
+
+	this.imageFileNames = [];
+	this.images = [];
+	this.outPrefix = null; //"out/test";
+	this.templateFile = __dirname + "/../tpl/leaflet.tpl";
+}
+
+function pngReadPixelRgb(png, x, y) {
+	return {
+		r: png.data[(y * png.width + x) * 4],
+		g: png.data[(y * png.width + x) * 4 + 1],
+		b: png.data[(y * png.width + x) * 4 + 2],
+	};
+}
+
+function pngReadPixelBool(png, x, y) {
+	var rgb = pngReadPixelRgb(png, x, y);
+
+	return ((rgb.r + rgb.g + rgb.b) / 3) > 127;
+}
+
+function range(numItems) {
+	var res = [];
+
+	for (var i = 0; i < numItems; i++)
+		res.push(i);
+
+	return res;
+}
+
+StaduimPixels.prototype.addImageFile = function(fn) {
+	this.imageFileNames.push(fn);
+}
+
+StaduimPixels.prototype.setOutPrefix = function(prefix) {
+	this.outPrefix = prefix;
+}
+
+StaduimPixels.prototype.setTargetWidth = function(v) {
+	this.targetWidth = parseInt(v);
+}
+
+StaduimPixels.prototype.setTargetHeight = function(v) {
+	this.targetHeight = parseInt(v);
+}
+
+StaduimPixels.prototype.setTemplateFile = function(v) {
+	this.templateFile = v;
+}
+
+StaduimPixels.prototype.run = function() {
+	this.runThenable = new Thenable();
+
+	var images = [];
+
+	if (!this.outPrefix) {
+		this.runThenable.reject("outPrefix needs to be specified.");
+		return this.runThenable;
+	}
+
+	for (var i = 0; i < this.imageFileNames.length; i++) {
+		var data = fs.readFileSync(this.imageFileNames[i]);
+		var png = PNG.sync.read(data);
+		/*if (png.width != this.targetWidth || png.height != this.targetHeight) {
+			var e = "wrong size, expected " + this.targetWidth + "x" + this.targetHeight;
+			this.runThenable.reject(e);
+			return this.runThenable;
+		}*/
+
+		images.push(png);
+	}
+
+	var index = 0;
+	var pageNum = 0;
+	var vars = {
+		pages: []
+	};
+
+	async.eachSeries(range(this.targetHeight), function(y, yCallback) {
+		async.eachSeries(range(this.targetWidth), function(x, xCallback) {
+			var page = {};
+
+			page.instructions = [];
+			page.row = (y + 1);
+			page.seat = (x + 1);
+
+			for (i = 0; i < images.length; i++) {
+				var image = images[i];
+
+				page.instructions.push({
+					program: (i + 1),
+					value: pngReadPixelBool(image, x, y)
+				});
+			}
+
+			vars.pages.push(page);
+
+			index++;
+
+			if (index == 4) {
+				var options = {
+					siteType: 'html',
+					screenSize: {
+						width: 670,
+						height: 870
+					}
+				}
+
+				var content = swig.renderFile(this.templateFile, vars);
+				console.log("creating page " + pageNum);
+
+				webshot(content, this.outPrefix + pageNum + '.png', options,
+					function(err) {
+						if (err)
+							console.log(err);
+
+						index = 0;
+						pageNum++;
+						vars = {
+							pages: []
+						};
+						xCallback();
+					});
+			} else {
+				xCallback();
+			}
+		}.bind(this), yCallback);
+	}.bind(this), this.runThenable.resolve.bind(this.runThenable));
+
+	return this.runThenable;
+}
+
+module.exports = StaduimPixels;
